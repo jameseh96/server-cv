81c682bf0e1a596efbdc99eadaef8b2e843c11bc
Dave Rigby <daver@couchbase.com>
2020-06-30 11:20:50 +0100

    MB-40218: macOS: Disable RocksDB support
    Disable RocksDB support on macOS mad-hatter - at least two tests are fail frequently:
    
        Running [0001/0157]: expiry (couchstore).....
        /kv_engine/engines/ep/tests/ep_testsuite.cc:869 Test failed: `' (Expected `1', got `2' - curr_items stat should be still 1 after overwriting the key that was expired, but not purged yet)
        (62 ms) FAIL
    
        [ RUN      ] AllBackends/DurabilityEPBucketTest.CompactionOfPrepareDoesNotAddToBloomFilter/persistentRocksdb_value_only
        unknown file: Failure
        C++ exception with description "CheckedExecutor failed fetchNextTask" thrown in the test body.
        [  FAILED  ] AllBackends/DurabilityEPBucketTest.CompactionOfPrepareDoesNotAddToBloomFilter/persistentRocksdb_value_only, where GetParam() = ("persistentRocksdb", "value_only") (15 ms)
    
    Change-Id: I1a3d9608980a123ddf725b26fe14263d72991d8e
    Reviewed-on: http://review.couchbase.org/c/server-cv/+/131623
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: James Harrison <james.harrison@couchbase.com>

--- jenkins-jobs/config/kv_engine/macos.groovy
+++ jenkins-jobs/config/kv_engine/macos.groovy
@@ -11,4 +11,8 @@ TESTS_EXCLUDE="memcached-spdlogger-test"
 // Disable magma support - we don't ship with it in mad-hatter and it is
 // currently failing on macOS XCode 9.3
 CMAKE_ARGS="${CMAKE_ARGS} " + "-DEP_USE_MAGMA=0"
+
+// Disable RocksDB support - we don't ship with it in mad-hatter and some of
+// the tests are intermittently failing.
+CMAKE_ARGS="${CMAKE_ARGS} " + "-DEP_USE_ROCKSDB=0"
 }
